61390cc6314fb45826af005605e40efb3a25503a
Phạm Phú Quý Đôn <donezombie@gmail.com>
2021-01-11 15:22:48 +0700

    Update root saga

--- src/redux/root.ts
+++ src/redux/root.ts
@@ -1,10 +1,28 @@
 import { combineReducers } from 'redux';
-import { fork, all } from 'redux-saga/effects';
+import { all, spawn, call } from "redux-saga/effects";
 import { authSaga, authReducer } from './modules/auth';
 import { todosReducer, todosSaga } from './modules/todos';
 
 export function* rootSagas() {
-  yield all([fork(authSaga), fork(todosSaga)]);
+  const sagas = [
+    authSaga,
+    todosSaga,
+  ];
+
+  yield all(
+    sagas.map(saga =>
+      spawn(function*() {
+        while (true) {
+          try {
+            yield call(saga);
+            break;
+          } catch (e) {
+            console.log(e);
+          }
+        }
+      })
+    )
+  );
 }
 
 export const rootReducers = combineReducers({
